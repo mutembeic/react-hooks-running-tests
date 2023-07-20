b90d2b92b1a32a812c0d2c72caaaf116ea6c00fc
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-07-20 10:29:00 +0300

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ function Header(props) {
       return (
         <h1>
           {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+          {
+             <h1>hello from the Header!</h1>
+}
+
         </h1>
       )
 }
